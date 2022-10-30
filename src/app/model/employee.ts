ca5e7f38d08d023f077c9e2368a5b9695ec9cdf2
Muhammad Iqbal Aditama <iqbaladitama91@gmail.com>
2022-10-30 20:18:05 +0700

    change ? variable to !

--- src/app/model/employee.ts
+++ src/app/model/employee.ts
@@ -1,12 +1,12 @@
 export class Employee {
     id!: number
-    username?: string
-    firstName?: string
-    lastName?: string
+    username!: string
+    firstName!: string
+    lastName!: string
     email!: string
     basicSalary!: number
     birtDate!: string
     group!: string
-    status?: string
-    description?: string
+    status!: string
+    description!: string
 }
