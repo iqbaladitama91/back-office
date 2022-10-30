80ccb0c0557d3a89acccc5195950a7bb43ec807b
Muhammad Iqbal Aditama <iqbaladitama91@gmail.com>
2022-10-30 22:57:23 +0700

    add default value to employee model

--- src/app/model/employee.ts
+++ src/app/model/employee.ts
@@ -1,12 +1,12 @@
 export class Employee {
     id!: number
-    username!: string
-    firstName!: string
-    lastName!: string
-    email!: string
-    basicSalary!: number
-    birtDate!: string
-    group!: string
-    status!: string
-    description!: string
+    username: string = ""
+    firstName: string = ""
+    lastName: string = ""
+    email: string = ""
+    basicSalary: number = 0
+    birtDate: string = ""
+    group: string = ""
+    status: string = ""
+    description: string = ""
 }
