5e425c368776a07c273e2c7b1f93ce32bf13ac0c
Muhammad Iqbal Aditama <iqbaladitama91@gmail.com>
2022-10-30 17:53:52 +0700

    add dummy edit and delete function on employee list page

--- src/app/pages/employee-list/employee-list.component.ts
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import data from '../../../data/mock.json'
 import { Employee } from 'src/app/model/employee';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,30 +12,34 @@ import { Employee } from 'src/app/model/employee';
 export class EmployeeListComponent implements OnInit {
 
   employeeList : Employee[] = data
+  email: string = ""
+  group: string = ""
   optionPerPageList :number[] = [5,10,15,20]
-  email?: any
-  group?: any
   reverse: boolean = false
   key: string = "id"
   p: number = 1
   itemPerPage: number = 5
 
-  constructor() { }
 
-  ngOnInit(): void {    
+  constructor(private router : Router, private toast: ToastrService) { }
+
+  ngOnInit(): void {  
+    console.log(this.employeeList);
+      
   }
 
   search(): void {
-    if(this.email !== undefined && this.group !== undefined ){
+    const email = this.email
+    const group = this.group
+    if(email.length > 0 && group.length > 0 ){
       this.employeeList = this.employeeList.filter( el => {
-        return el.email?.toLocaleLowerCase().match(this.email?.toLocaleLowerCase()) 
-        && el.group?.toLocaleLowerCase().match(this.group.toLocaleLowerCase());
+        return el.email?.toLocaleLowerCase().match(email.toLocaleLowerCase()) 
+        && el.group?.toLocaleLowerCase().match(group.toLocaleLowerCase());
       })
     } else {
-      console.log(this.email !== undefined, this.group !== undefined);
+      console.log(email !== undefined, group !== undefined);
       
       this.employeeList = data
-      console.log("Salahhhh");
     }
   }
   reset(): void {
@@ -42,5 +48,12 @@ export class EmployeeListComponent implements OnInit {
   sort(key: any) : void {
     this.key = key
     this.reverse= !this.reverse
+  }
+
+  edit() : void {
+    this.toast.warning("Edit Data Success", "Edit Action")
+  }
+  delete() : void {
+    this.toast.error("Delete Data Success", "Delete Action")
   }
 }
